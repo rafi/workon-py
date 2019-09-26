232d6025127721a5a1a5fdc70bb301ffa13ad4f0
Rafael Bodill <justrafi@gmail.com>
2019-09-26 11:33:32 +0300

    Add support for fzf/fzy selection and activation

--- workon-completion.bash
+++ workon-completion.bash
@@ -1,29 +1,29 @@
 # workon-py - work on virtualenvs via subshell
 # http://github.com/rafi/workon-py
 #
-# Copyright (C) 2015-2016 Rafael Bodill <justrafi at gmail>
+# Copyright (C) 2015-2019 Rafael Bodill <justrafi at gmail>
 # Distributed under the GNU General Public License, version 2.0.
 
 cdvirtualenv () {
-	cd "$(_workon_prefix)${1}"
+	cd "$(__workon_prefix)${1}" || echo 'Unable to change directory'
 }
 
-_workon_prefix () {
+__workon_prefix () {
 	local envs="${WORKON_HOME:-${XDG_DATA_HOME:-$HOME/.local/share}/python/envs}"
 	echo "${envs%/}/"
 }
 
-_workon_complete () {
+__workon_complete () {
 	COMPREPLY=()
 	local cur="${COMP_WORDS[COMP_CWORD]}"
-	prefix="$(_workon_prefix)"
+	prefix="$(__workon_prefix)"
 
 	local IFS=$'\n'
-	local items=($(compgen -d $prefix$cur))
-	for item in ${items[@]}; do
+	local items=( $(compgen -d "$prefix$cur") )
+	for item in "${items[@]}"; do
 		[[ $item == $prefix.* ]] && continue
 		COMPREPLY+=("${item#$prefix}")
 	done
 }
 
-complete -o filenames -o nospace -F _workon_complete workon cdvirtualenv
+complete -o filenames -o nospace -F __workon_complete workon cdvirtualenv
